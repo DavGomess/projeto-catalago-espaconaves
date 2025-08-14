9445de677e7de61d58bf9ae8da0994765824e9f5
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2025-08-14 18:42:35 -0300

    ajuste no titulo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
       <div className="barraNav">
-        <h2>EXERCÍCIO 3-CATÁLOGO DE ESPAÇONAVES</h2>
+        <h2>CATÁLOGO DE ESPAÇONAVES</h2>
         <nav>
           <Abas/>
         </nav>
